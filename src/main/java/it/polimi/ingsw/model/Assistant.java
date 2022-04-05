d6cae1f7347e29415d371c125d05bc3739de168f
magicteo <matteo.macaluso@mail.polimi.it>
2022-04-05 14:37:21 +0200

    Added Assistant constructors and methods

--- src/main/java/it/polimi/ingsw/model/Assistant.java
+++ src/main/java/it/polimi/ingsw/model/Assistant.java
@@ -1,7 +1,37 @@
 package it.polimi.ingsw.model;
 
 public enum Assistant {
+    TIGER(1,1),
+    OSTRICH(2,1),
+    ARCHER_CAT(3,2),
+    EAGLE(4,2),
+    FOX(5,3),
+    SNAKE(6,3),
+    OCTOPUS(7,4),
+    DOG(8,4),
+    ELEPHANT(9,5),
+    TURTLE(10,5);
 
 
+    private final int value;
+    private final int movements;
+
+
+    Assistant(int value, int movements){
+        this.value = value;
+        this.movements = movements;
+    }
+
+    public boolean equals(Assistant assistant){
+        return assistant.value == this.value && assistant.movements == this.movements;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public int getMovements(){
+        return this.movements;
+    }
 
 }
