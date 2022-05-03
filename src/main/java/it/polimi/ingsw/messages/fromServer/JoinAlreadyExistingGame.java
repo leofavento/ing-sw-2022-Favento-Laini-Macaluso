5a50f8bbdb38d4e375d4615adf20db6e42cc8ff9
magicteo <matteo.macaluso@mail.polimi.it>
2022-05-03 21:01:54 +0200

    Updated JoinAlreadyExistingGame

--- src/main/java/it/polimi/ingsw/messages/fromServer/JoinAlreadyExistingGame.java
+++ src/main/java/it/polimi/ingsw/messages/fromServer/JoinAlreadyExistingGame.java
@@ -1,27 +1,19 @@
 package it.polimi.ingsw.messages.fromServer;
 
 import it.polimi.ingsw.messages.Message;
-import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.GameInfo;
 
 public class JoinAlreadyExistingGame implements Message {
 
-    private int gameID;
-    private int numOfWaitingPlayers;
-    private int numOfTotalPlayers;
-    private boolean expertGame;
+    private GameInfo gameInfo;
 
 
-    public JoinAlreadyExistingGame(int gameID, int numOfWaitingPlayers, int numOfTotalPlayers, boolean expertGame){
-        this.gameID = gameID;
-        this.numOfWaitingPlayers = numOfWaitingPlayers;
-        this.numOfTotalPlayers = numOfTotalPlayers;
-        this.expertGame = expertGame;
+    public JoinAlreadyExistingGame(){
+        gameInfo = new GameInfo(gameInfo.getGameID(), gameInfo.getNumOfWaitingPlayers(), gameInfo.getNumOfTotalPlayers(), gameInfo.isExpertGame());
 
 
     }
 
-    GameInfo gameInfo = new GameInfo(gameID, numOfWaitingPlayers, numOfTotalPlayers, expertGame);
 
     public GameInfo getGameInfo(){
         return gameInfo;
