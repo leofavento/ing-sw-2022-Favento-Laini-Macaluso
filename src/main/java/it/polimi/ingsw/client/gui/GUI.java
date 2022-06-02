bc127eb8935b423b7fa929381412fceaf387d14c
leofavento <100695116+leofavento@users.noreply.github.com>
2022-06-02 19:11:34 +0200

    Added JavaFX in pom

---
+++ src/main/java/it/polimi/ingsw/client/gui/GUI.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.client.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
