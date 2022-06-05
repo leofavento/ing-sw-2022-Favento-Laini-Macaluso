5e3bd0a58b2ad8acf3a929cef00fec9aa0f62fe7
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2022-06-05 21:30:07 +0200

    Added newGame scene

---
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/initial/newGameController.java
@@ -0,0 +1,70 @@
+package it.polimi.ingsw.client.gui.controllers.initial;
+
+import it.polimi.ingsw.client.gui.GUI;
+import it.polimi.ingsw.client.gui.controllers.Controller;
+import it.polimi.ingsw.messages.fromClient.SetGame;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class newGameController implements Controller, Initializable {
+
+    private GUI gui;
+
+    @FXML
+    private CheckBox myCheckBox;
+    private final Integer[] numPlayers = {2, 3, 4};
+    @FXML private Label errorMessage;
+
+    @FXML
+    private Label modeLabel;
+    @FXML
+    private ChoiceBox<Integer> numberChoiceBox;
+
+
+    @Override
+    public void setGui(GUI gui) {
+        this.gui = gui;
+    }
+
+    @Override
+    public void error(String error) {
+        errorMessage.setText(error);
+        errorMessage.setVisible(true);
+    }
+
+    @Override
+    public void nextPhase() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        numberChoiceBox.getItems().addAll(numPlayers);
+    }
+
+    public void change(ActionEvent event){
+        if (myCheckBox.isSelected()){
+            modeLabel.setText("You will play with Expert mode rules.");
+        }
+        else {
+            modeLabel.setText("You will play with Normal mode rules.");
+        }
+    }
+
+    public void createGame(){
+        if (numberChoiceBox.getValue()==null){
+            errorMessage.setText("Please select the number of players.");
+            errorMessage.setVisible(true);
+        }
+        else {
+            gui.getClient().sendMessage(new SetGame(numberChoiceBox.getValue(), myCheckBox.isSelected()));
+        }
+    }
+}
