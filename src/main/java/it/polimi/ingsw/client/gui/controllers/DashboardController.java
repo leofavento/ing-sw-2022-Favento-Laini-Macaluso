fbe8e6466381895d124bf56e64104bb60c64a290
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2022-06-07 17:36:20 +0200

    First dashboard commit

---
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/DashboardController.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.client.gui.controllers;
+
+import it.polimi.ingsw.client.gui.GUI;
+
+public class DashboardController implements Controller{
+
+    private GUI gui;
+
+    @Override
+    public void setGui(GUI gui) {
+        this.gui = gui;
+    }
+
+    @Override
+    public void error(String error) {
+
+    }
+
+    @Override
+    public void nextPhase() {
+
+    }
+}
